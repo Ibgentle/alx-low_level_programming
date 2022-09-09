c84d1c57ad8c439a6e50d22ef11bc356258419f5
ibor offor <iboroffor@gmail.com>
2022-09-09 10:34:56 -0700

    positive or negative number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,33 +13,26 @@
  * Return: 0 (if successful)
  *
  */
-
 int main(void)
 {
+	int n;
 
-		int n;
+	srand(time(0));
 
+	n = rand() - RAND_MAX / 2;
 
+	/* your code goes there */
 
-		srand(time(0));
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+	{
+		printf("Something went wrong");
+	}
 
-		n = rand() - RAND_MAX / 2;
-
-		/* your code goes there */
-
-		if (n > 0)
-			printf("%d is positive\n", n);
-		else if (n == 0)
-			printf("%d is zero\n", n);
-		else if (n < 0)
-			printf("%d is negative\n", n);
-		else 
-		{
-			printf("Something went wrong");
-		}
-
-
-
-		return (0);
-
+	return (0);
 }
