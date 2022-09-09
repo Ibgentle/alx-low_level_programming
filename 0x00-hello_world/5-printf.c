9049ef4b5c0256a0a0f3c07980db34b9ebdf213c
ibor offor <iboroffor@gmail.com>
2022-09-09 04:00:52 -0700

    with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <std.io>
+/**
+ * main - Entry point: this is where execution starts
+ *
+ * Return: Always 0 (if successful)
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+
+}
