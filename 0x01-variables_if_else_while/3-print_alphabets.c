aa28226e3f493b3b89b1d2326041a512a240dc3b
ibor offor <iboroffor@gmail.com>
2022-09-10 03:31:13 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: this program prints lowercase alphabets
+ *
+ * Return: Always 0 (if successful)
+ *
+ */
+int main(void)
+{
+	int small_alph;
+
+	int big_alph;
+
+	for (small_alph = 'a'; small_alph <= 'z'; small_alph++)
+		putchar(small_alph);
+	for (big_alph = 'A'; big_alph <= 'Z'; big_alph++)
+		putchar(big_alph);
+	putchar('\n');
+	return (0);
+}
