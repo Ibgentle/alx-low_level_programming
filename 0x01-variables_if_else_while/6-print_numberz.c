07a07bf9325e8027c04335fdbaa338110f55eaee
ibor offor <iboroffor@gmail.com>
2022-09-10 07:08:50 -0700

    Print single digit base 10 numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:this program prints single-digit base ten numbers using putchar
+ *
+ * Return: 0 (if successful)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number < '10'; number++)
+		putchar(number);
+
+	putchar('\n');
+	return (0);
+}
