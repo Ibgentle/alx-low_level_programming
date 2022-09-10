76779ba99be1832939192dd6c91f55b1455116f6
ibor offor <iboroffor@gmail.com>
2022-09-10 07:23:01 -0700

    Print single digit base 10 numbers using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,8 +10,11 @@ int main(void)
 {
 	int number;
 
-	for (number = '0'; number < '10'; number++)
+	for (number = 0; number < 10; number++)
+	{
+		(char)number;
 		putchar(number);
+	}
 
 	putchar('\n');
 	return (0);
