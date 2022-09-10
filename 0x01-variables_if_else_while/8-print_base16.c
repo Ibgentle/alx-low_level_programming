76e3369c19258a30e4038e7fbc34f17f2c36ed8c
ibor offor <iboroffor@gmail.com>
2022-09-10 09:53:23 -0700

    Print all base16 numbers in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 	int num_b16;
 	int alph_b16;
 
-	for (base_16 = 0; base_16 < 16; base_16++)
+	for (num_b16 = 0; num_b16 < 16; num_b16++)
 		putchar((number % 10) + '0');
 	for (alph_b16 = 'a'; alph_b16 <= 'f'; alph_b16++)
 		putchar(alph_b16);
