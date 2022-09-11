7abdf7d46201ee220a3f49cb3d5336fb2163c07f
ibor offor <iboroffor@gmail.com>
2022-09-11 15:49:37 -0700

    Two digit unique combos of single digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,16 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = num1; num2 < 10; num2++)
 		{
+			if (num1 >= num2)
+				continue;
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
-			if (num1 == 9 && num2 == 9)
-				continue;
+			if (num1 == 8 && num2 == 9)
+				break;
 			putchar(',');
 			putchar(' ');
 		}
