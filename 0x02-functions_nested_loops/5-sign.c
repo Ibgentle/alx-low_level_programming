8051fd7ce38e1a36defd3efe9966eef6a36ad572
ibor offor <iboroffor@gmail.com>
2022-09-13 14:29:43 -0700

    Check the sign of an argument

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,23 +4,23 @@
  *
  * Description: prints + for positive, 0 for zero and -1 for negative arguments
  *
- * @c: parameter holding argument to be checked
+ * @n: parameter holding argument to be checked
  *
  * Return: 0, 1 or -1 (if successful)
  */
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c >= 1)
+	if (n >= 1)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (c == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else if (c < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
