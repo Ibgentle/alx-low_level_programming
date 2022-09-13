6dfd1be1075152a51ff67b597bb3f05771c03be8
ibor offor <iboroffor@gmail.com>
2022-09-13 12:06:11 -0700

    print the alphabet 10 times with a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - writes the alphabet
  *
- * Description: this prints the alphabet 10 times, in lowercase, followed by a new line.
+ * Description: this prints the alphabet 10 times, followed by a new line.
  *
  * Return: null
  *
@@ -11,19 +11,18 @@ void print_alphabet_x10(void)
 {
 	char alph;
 	int lines;
+
 	lines = 0;
+
 
 	while (lines <= 10)
 	{
 		for (alph = 'a'; alph <= 'z'; alph++)
-		{
 			_putchar(alph);
-		
-		}
+
 		_putchar('\n');
 		lines++;
 	}
-	_putchar('\n');
 	return;
 
 }
