9baf3396708c68d88f9ec42e18ed28a352296124
ibor offor <iboroffor@gmail.com>
2022-09-14 12:26:17 -0700

    add two integer values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add two numbers and return the result
+ *
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: result of a added to b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
