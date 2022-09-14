83eea03cdd3bdf53d298a7d7b176cc509b005dfb
ibor offor <iboroffor@gmail.com>
2022-09-14 03:08:47 -0700

    Print last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,14 @@
  *
  * @n: the parameter holds the argument to be returned
  *
- * Return: 0 (if successful
+ * Return: last digit of n (if successful)
  */
 int print_last_digit(int n)
 {
+	int last_digit;
+
+
 	_putchar((n % 10) + '0');
-	return (0);
+	last_digit = _putchar((n % 10) + '0');
+	return (last_digit);
 }
