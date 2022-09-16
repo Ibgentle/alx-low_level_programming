535e484d2421618664a97aeb3df9b14caa5e5573
ibor offor <iboroffor@gmail.com>
2022-09-16 16:10:39 -0700

    Function to checks case of alphabet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - checks for uppercase alphabet
+ *
+ * Return: 1 if upper, 0 otherwise
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory is about More functions, more nested loops
