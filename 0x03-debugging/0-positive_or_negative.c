d9d7e759f827ef9992f73949b2fd99b1440ed1db
ibor offor <iboroffor@gmail.com>
2022-09-16 17:31:04 -0700

    Removed cause of infinite loop

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 /**
-* positive_or_negative - checks if a number is positive or negative
-*
-* @i - the integer parameter to be checked
-*
-* Return: void
-*/
-
+ * positive_or_negative - checks the number
+ *
+ * @i: the integer parameter to be checked
+ *
+ * Return: void
+ */
 void positive_or_negative(int i)
 {
-        if (i > 0)
-                printf("%d is positive\n", i);
-        else if (i == 0)
-                printf("%d is zero\n", i);
-        else if (i < 0)
-                printf("%d is negative\n", i);
-
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
 }
 
