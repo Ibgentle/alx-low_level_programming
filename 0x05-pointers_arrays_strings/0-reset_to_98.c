4ed583cc90450dada76f559e9f998493f146a9dc
ibor offor <iboroffor@gmail.com>
2022-09-20 01:34:01 -0700

    Update the integer to 98 using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - updtate the vlaue of the integer to 98
+ *
+ * @*n: the pointer to the integer to be updated
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
