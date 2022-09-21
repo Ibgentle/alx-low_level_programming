f51c6be27ec88ed79126d0d712d734bc414955fe
ibor offor <iboroffor@gmail.com>
2022-09-20 20:42:52 -0700

    Print a reversed string followed by a newline

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,10 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0;
+	int index = 0;
 
-	while (s[index])
+	while (*s[index++])
 		len++;
 
 	for (index = len - 1; index >= 0; index--)
