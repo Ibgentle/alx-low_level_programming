1cdfb51fd734554f61e7e9132d185ac6b63b4e8f
ibor offor <iboroffor@gmail.com>
2022-09-20 21:01:33 -0700

    Print every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: string whose characters are printed
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int index = 0;
+	char tmp;
+
+	while (s[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
+
